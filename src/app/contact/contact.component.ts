2a2a6042179fd14ff814c4b3232c125413728560
MalikBenJemiaa <melekbenjemiaa12@gmail.com>
2023-04-30 19:21:22 +0200

    'lastVersion'

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  wwww=[{percent:70},{percent:10},{percent:80},{percent:41},{percent:56}
+    ,{percent:36},{percent:56},{percent:30},{percent:40},{percent:50},{percent:96}
+    ,{percent:35},{percent:14},{percent:20},{percent:50},{percent:40},{percent:45},{percent:50},
+    {percent:25},{percent:78},{percent:20},{percent:60},{percent:52},{percent:21},
+    {percent:15},{percent:65},{percent:10},{percent:70},{percent:51},{percent:45},{percent:50},
+    {percent:75},{percent:99},{percent:10},{percent:80},{percent:23},{percent:12},{percent:50},
+    {percent:45},{percent:23}]
+  len=this.wwww.length
 }
